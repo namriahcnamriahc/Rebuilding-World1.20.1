42a5007afd369a804e84821148dda1c409ea86bc
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-03-16 23:37:41 +0900

    Add "rebuilding_world_series"tag.

---
+++ src/main/java/com/namriahc/Rebuilding_World/registry/Rebuilding_World_Tags.java
@@ -0,0 +1,25 @@
+package com.namriahc.Rebuilding_World.registry;
+
+import com.namriahc.Rebuilding_World.main.Rebuilding_World;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class Rebuilding_World_Tags {
+
+    public static class Blocks{
+        private static TagKey<Block> tag (String name){
+            return BlockTags.create(new ResourceLocation(Rebuilding_World.MOD_ID,name));
+        }
+    }
+
+    public static class Items{
+        public static final TagKey<Item> REBUILDING_WORLD_SERIES = tag("rebuilding_world_series");
+        private static TagKey<Item> tag(String name){
+            return ItemTags.create(new ResourceLocation(Rebuilding_World.MOD_ID,name));
+        }
+    }
+}
