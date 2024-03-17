5aab6422ba695c5a5d7206bdf6d31db26dbe138c
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-03-17 12:22:06 +0900

    Add shell rock.ep.2

--- src/main/java/com/namriahc/Rebuilding_World/registry/tab/Tab_Rebuilding_World_Items.java
+++ src/main/java/com/namriahc/Rebuilding_World/registry/tab/Tab_Rebuilding_World_Items.java
@@ -1,5 +1,6 @@
 package com.namriahc.Rebuilding_World.registry.tab;
 
+import com.namriahc.Rebuilding_World.registry.Rebuilding_World_Blocks;
 import com.namriahc.Rebuilding_World.registry.Rebuilding_World_Items;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
@@ -9,6 +10,7 @@ public class Tab_Rebuilding_World_Items {
     public static final Item[] items = {
             Items.ITEM_FRAME,
             Items.BARRIER,
-            Rebuilding_World_Items.STEELY_BARK.get()
+            Rebuilding_World_Items.STEELY_BARK.get(),
+            Rebuilding_World_Blocks.BlockItems.SHELL_ROCK.get()
     };
 }
