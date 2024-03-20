fb2ccac3c7828a7d3ea8f82bab08c6b88fb3af51
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-03-20 22:45:44 +0900

    Add sponge sand season2.ep.2

--- src/main/java/com/namriahc/Rebuilding_World/block/custom/Block_sponge_sand.java
+++ src/main/java/com/namriahc/Rebuilding_World/block/custom/Block_sponge_sand.java
@@ -1,7 +1,12 @@
 package com.namriahc.Rebuilding_World.block.custom;
-
-import net.minecraft.world.level.block.FallingBlock;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 
 public class Block_sponge_sand extends FallingBlock {
     public Block_sponge_sand() {
@@ -9,4 +14,11 @@ public class Block_sponge_sand extends FallingBlock {
                 .sound(SoundType.SAND)
         );
     }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        pLevel.setBlock(pPos, Blocks.WET_SPONGE.defaultBlockState(),2);
+        return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+    }
+
 }
