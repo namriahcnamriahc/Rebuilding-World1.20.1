2c5a35427bb482839d8409236db657851c55cc60
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-03-25 01:15:40 +0900

    Add datagen.season2.ep.3

--- src/main/java/com/namriahc/Rebuilding_World/datagen/DataGenerator.java
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/DataGenerator.java
@@ -19,6 +19,7 @@ public class DataGenerator {
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput));
         generator.addProvider(event.includeServer(), ModLootTableProvider.create(packOutput));
 
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
