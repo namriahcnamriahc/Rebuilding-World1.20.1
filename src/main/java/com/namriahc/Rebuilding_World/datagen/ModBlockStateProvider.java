486fdb2ca7badebc9422c2d612c1ba2d65b20b45
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-05-06 01:50:10 +0900

    Add Datagen season2.ep.2.

--- src/main/java/com/namriahc/Rebuilding_World/datagen/ModBlockStateProvider.java
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/ModBlockStateProvider.java
@@ -1,4 +1,17 @@
 package com.namriahc.Rebuilding_World.datagen;
 
-public class ModBlockStateProvider {
+import com.namriahc.Rebuilding_World.main.Rebuilding_World;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, Rebuilding_World.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+
+    }
 }
