26add3f384acb6273fe8b9e315f1f7bbb7225fc7
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-07-09 19:36:23 +0900

    Add Datagen season2.ep.5.

--- src/main/java/com/namriahc/Rebuilding_World/datagen/ModBlockStateProvider.java
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/ModBlockStateProvider.java
@@ -1,9 +1,12 @@
 package com.namriahc.Rebuilding_World.datagen;
 
 import com.namriahc.Rebuilding_World.main.Rebuilding_World;
+import com.namriahc.Rebuilding_World.registry.Rebuilding_World_Blocks;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockStateProvider extends BlockStateProvider {
     public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
@@ -12,6 +15,12 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
+        blockWithItem(Rebuilding_World_Blocks.Blocks.FIRST_SOIL);
+        blockWithItem(Rebuilding_World_Blocks.Blocks.SHELL_ROCK);
 
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
     }
 }
