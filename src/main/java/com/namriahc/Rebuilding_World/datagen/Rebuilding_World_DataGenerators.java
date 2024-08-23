4a4dd9635aac3a7bdb9801139e0f9a0e48f23fc3
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-08-23 17:48:35 +0900

    Add new Datagen season3 ep.2

---
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_DataGenerators.java
@@ -0,0 +1,10 @@
+package com.namriahc.Rebuilding_World.datagen;
+
+import com.namriahc.Rebuilding_World.main.Rebuilding_World;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Rebuilding_World.MOD_ID ,bus = Mod.EventBusSubscriber.Bus.MOD)
+
+public class Rebuilding_World_DataGenerators {
+    public static void
+}
--- src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_ItemModelProvider.java
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_ItemModelProvider.java
@@ -1,6 +1,7 @@
 package com.namriahc.Rebuilding_World.datagen;
 
 import com.namriahc.Rebuilding_World.main.Rebuilding_World;
+import com.namriahc.Rebuilding_World.registry.Rebuilding_World_Items;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -11,5 +12,7 @@ public class Rebuilding_World_ItemModelProvider extends net.minecraftforge.clien
 
     @Override
     protected void registerModels() {
+        basicItem(Rebuilding_World_Items.STEELY_BARK.get());
+        basicItem(Rebuilding_World_Items.STEELY_TREE_SEED_FOSSIL.get());
     }
 }
