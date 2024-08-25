f618971c9c993b32ef69e060622af8dd764fc694
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-08-25 15:49:17 +0900

    Add new Datagen season3 ep.3

--- src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_DataGenerators.java
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_DataGenerators.java
@@ -1,10 +1,46 @@
 package com.namriahc.Rebuilding_World.datagen;
 
 import com.namriahc.Rebuilding_World.main.Rebuilding_World;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Rebuilding_World.MOD_ID ,bus = Mod.EventBusSubscriber.Bus.MOD)
 
 public class Rebuilding_World_DataGenerators {
-    public static void
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        generator.addProvider(event.includeClient(), new Rebuilding_World_ItemModelProvider(packOutput
+                , existingFileHelper));
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
