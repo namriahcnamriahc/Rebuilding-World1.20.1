e28cb9a59093b3267157e213afd9d693a9baf580
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-08-27 17:37:54 +0900

    Add new Datagen season3 ep.12

--- src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_ItemModelProvider.java
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_ItemModelProvider.java
@@ -3,7 +3,11 @@ package com.namriahc.Rebuilding_World.datagen;
 import com.namriahc.Rebuilding_World.main.Rebuilding_World;
 import com.namriahc.Rebuilding_World.registry.Rebuilding_World_Items;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
 
 public class Rebuilding_World_ItemModelProvider extends net.minecraftforge.client.model.generators.ItemModelProvider {
     public Rebuilding_World_ItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
@@ -22,5 +26,34 @@ public class Rebuilding_World_ItemModelProvider extends net.minecraftforge.clien
         basicItem(Rebuilding_World_Items.SHELL_STONE_SHOVEL.get());
 
         basicItem(Rebuilding_World_Items.PIONEERS_CONGEE.get());
+
+    }
+
+    private ItemModelBuilder handheldItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(Rebuilding_World.MOD_ID,"item/" + item.getId().getPath()));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
