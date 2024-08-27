0b3a9fab8fdf98e31658c7a2fdf1509c74903c76
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-08-27 23:08:08 +0900

    Add new Datagen season3 ep.13

--- src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_ItemModelProvider.java
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_ItemModelProvider.java
@@ -4,7 +4,9 @@ import com.namriahc.Rebuilding_World.main.Rebuilding_World;
 import com.namriahc.Rebuilding_World.registry.Rebuilding_World_Items;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
@@ -21,20 +23,48 @@ public class Rebuilding_World_ItemModelProvider extends net.minecraftforge.clien
 
         basicItem(Rebuilding_World_Items.STEELY_BARK_HELMET.get());
 
-        basicItem(Rebuilding_World_Items.SHELL_STONE_SWORD.get());
-        basicItem(Rebuilding_World_Items.SHELL_STONE_PICKAXE.get());
-        basicItem(Rebuilding_World_Items.SHELL_STONE_SHOVEL.get());
+
+
+        handheldsworditem(Rebuilding_World_Items.SHELL_STONE_SWORD);
+        handheldpickaxeitem(Rebuilding_World_Items.SHELL_STONE_PICKAXE);
+        handheldshovelitem(Rebuilding_World_Items.SHELL_STONE_SHOVEL);
 
         basicItem(Rebuilding_World_Items.PIONEERS_CONGEE.get());
-
     }
 
-    private ItemModelBuilder handheldItem(RegistryObject<Item> item) {
+    private ItemModelBuilder handheldsworditem(RegistryObject<SwordItem> item) {
         return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation("item/handheld")).texture("layer0",
+                new ResourceLocation(Rebuilding_World.MOD_ID,"item/" + item.getId().getPath()));
+    }
+
+    private ItemModelBuilder handheldpickaxeitem(RegistryObject<PickaxeItem> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/handheld")).texture("layer0",
+                new ResourceLocation(Rebuilding_World.MOD_ID,"item/" + item.getId().getPath()));
+    }
+
+    private ItemModelBuilder handheldshovelitem(RegistryObject<ShovelItem> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/handheld")).texture("layer0",
                 new ResourceLocation(Rebuilding_World.MOD_ID,"item/" + item.getId().getPath()));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
