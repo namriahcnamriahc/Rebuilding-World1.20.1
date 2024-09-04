a20bdf07070a1db74822652dedf7b3a1eb5a334f
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-09-04 18:14:24 +0900

    Add new Datagen season3 ep.21

--- src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_LootTableProvider.java
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_LootTableProvider.java
@@ -3,7 +3,6 @@ package com.namriahc.Rebuilding_World.datagen;
 import com.namriahc.Rebuilding_World.datagen.loot.Rebuilding_World_BlockLootTables;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 
 import java.util.List;
--- src/main/java/com/namriahc/Rebuilding_World/datagen/loot/Rebuilding_World_BlockLootTables.java
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/loot/Rebuilding_World_BlockLootTables.java
@@ -5,12 +5,10 @@ import com.namriahc.Rebuilding_World.registry.Rebuilding_World_Items;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
-import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
 import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
@@ -19,7 +17,7 @@ import net.minecraftforge.registries.RegistryObject;
 import java.util.Set;
 
 public class Rebuilding_World_BlockLootTables extends BlockLootSubProvider {
-    protected Rebuilding_World_BlockLootTables() {
+    public Rebuilding_World_BlockLootTables() {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
 
