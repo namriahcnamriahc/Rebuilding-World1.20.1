9a2c462a2a081b7adc3351ecad5172d0bdcb4e27
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-09-05 17:29:24 +0900

    Add steely tree sapling ep.2

--- src/main/java/com/namriahc/Rebuilding_World/block/custom/Block_steely_tree_sapling.java
+++ src/main/java/com/namriahc/Rebuilding_World/block/custom/Block_steely_tree_sapling.java
@@ -8,13 +8,13 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
-public class Block_steely_tree_sample extends CropBlock {
+public class Block_steely_tree_sapling extends CropBlock {
 
     public static final int MAX_AGE = 7;
     public static final IntegerProperty AGE = BlockStateProperties.AGE_7;
 
-    public Block_steely_tree_sample(Properties p_52247_) {
-        super(p_52247_);
+    public Block_steely_tree_sapling() {
+        super(Properties.of());
     }
 
     @Override
