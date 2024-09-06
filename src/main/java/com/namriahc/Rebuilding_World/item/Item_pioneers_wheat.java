3156b6a9fe90ef1c9171c29156c54fbe20375ca2
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-09-06 12:16:56 +0900

    Add pioneer's wheat season1 ep.2

---
+++ src/main/java/com/namriahc/Rebuilding_World/item/Item_pioneers_wheat.java
@@ -0,0 +1,9 @@
+package com.namriahc.Rebuilding_World.item;
+
+import net.minecraft.world.item.Item;
+
+public class Item_pioneers_wheat extends Item {
+    public Item_pioneers_wheat() {
+        super(new Properties());
+    }
+}
--- src/main/java/com/namriahc/Rebuilding_World/registry/Rebuilding_World_Items.java
+++ src/main/java/com/namriahc/Rebuilding_World/registry/Rebuilding_World_Items.java
@@ -1,5 +1,6 @@
 package com.namriahc.Rebuilding_World.registry;
 
+import com.namriahc.Rebuilding_World.item.Item_pioneers_wheat;
 import com.namriahc.Rebuilding_World.item.Item_steely_bark;
 import com.namriahc.Rebuilding_World.item.Item_steely_tree_seed_fossil;
 import com.namriahc.Rebuilding_World.item.armor.Armor_steely_bark_boots;
@@ -32,6 +33,7 @@ public class Rebuilding_World_Items {
     public static final RegistryObject<ShovelItem> SHELL_STONE_SHOVEL = ITEMS.register("shell_stone_shovel", Tool_shell_stone_shovel::new);
 
     public static final RegistryObject<BowlFoodItem> PIONEERS_CONGEE = ITEMS.register("pioneers_congee", Food_pioneers_congee::new);
+    public static final RegistryObject<Item> PIONEERS_WHEAT = ITEMS.register("pioneers_wheat", Item_pioneers_wheat::new);
 
     public static final RegistryObject<Item> STEELY_TREE_SEED_FOSSIL = ITEMS.register("steely_tree_seed_fossil", Item_steely_tree_seed_fossil::new);
 
