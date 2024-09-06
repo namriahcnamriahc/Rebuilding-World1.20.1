d4508c6aa8f067a954f53ebb11aaa399e4ca28c5
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-09-06 18:02:24 +0900

    Add pioneer's wheat season1 ep.6

---
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_RecipeProvider.java
@@ -0,0 +1,50 @@
+package com.namriahc.Rebuilding_World.datagen;
+
+import com.namriahc.Rebuilding_World.main.Rebuilding_World;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Rebuilding_World_RecipeProvider extends RecipeProvider implements IConditionBuilder {
+    public Rebuilding_World_RecipeProvider(PackOutput p_248933_) {
+        super(p_248933_);
+    }
+    private static final List<ItemLike>
+
+    @Override
+    protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
+
+    }
+
+    protected static void oreSmelting(Consumer<FinishedRecipe> p_250654_, List<ItemLike> p_250172_, RecipeCategory p_250588_, ItemLike p_251868_, float p_250789_, int p_252144_, String p_251687_) {
+        oreCooking(p_250654_, RecipeSerializer.SMELTING_RECIPE, p_250172_, p_250588_, p_251868_, p_250789_, p_252144_, p_251687_, "_from_smelting");
+    }
+
+    protected static void oreBlasting(Consumer<FinishedRecipe> p_248775_, List<ItemLike> p_251504_, RecipeCategory p_248846_, ItemLike p_249735_, float p_248783_, int p_250303_, String p_251984_) {
+        oreCooking(p_248775_, RecipeSerializer.BLASTING_RECIPE, p_251504_, p_248846_, p_249735_, p_248783_, p_250303_, p_251984_, "_from_blasting");
+    }
+
+    protected static void oreCooking(Consumer<FinishedRecipe> p_250791_, RecipeSerializer<? extends AbstractCookingRecipe> p_251817_, List<ItemLike> p_249619_, RecipeCategory p_251154_, ItemLike p_250066_, float p_251871_, int p_251316_, String p_251450_, String p_249236_) {
+        Iterator var9 = p_249619_.iterator();
+
+        while(var9.hasNext()) {
+            ItemLike itemlike = (ItemLike)var9.next();
+            SimpleCookingRecipeBuilder.generic(Ingredient.of(new ItemLike[]{itemlike}), p_251154_, p_250066_,
+                    p_251871_, p_251316_, p_251817_)
+                    .group(p_251450_).unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(p_250791_, Rebuilding_World.MOD_ID + ":" + getItemName(p_250066_) + p_249236_ + "_" + getItemName(itemlike));
+        }
+
+    }
+}
