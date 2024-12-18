b68317b4596f0d9f617cf835c3a8bb62b368dc33
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2024-12-18 23:37:33 +0900

    Add steely_tree_sapling series ep.3

--- src/main/java/com/namriahc/Rebuilding_World/item/Item_steely_tree_seed_fossil.java
+++ src/main/java/com/namriahc/Rebuilding_World/item/Item_steely_tree_seed_fossil.java
@@ -1,12 +1,15 @@
 package com.namriahc.Rebuilding_World.item;
 
-import net.minecraft.world.item.Item;
+import com.namriahc.Rebuilding_World.registry.Rebuilding_World_Blocks;
+import net.minecraft.world.item.ItemNameBlockItem;
+import net.minecraft.world.item.Rarity;
 
-public class Item_steely_tree_seed_fossil extends Item {
+public class Item_steely_tree_seed_fossil extends ItemNameBlockItem {
     public Item_steely_tree_seed_fossil() {
-        super(new Properties()
-                .stacksTo(64)
-                .fireResistant()
-        );
+        super(Rebuilding_World_Blocks.Blocks.CROP_BLOCK_STEELY_TREE_SAPLING.get(),
+                new Properties()
+                        .fireResistant()
+                        .stacksTo(64)
+                        .rarity(Rarity.RARE));
     }
 }
--- src/main/java/com/namriahc/Rebuilding_World/registry/Rebuilding_World_Items.java
+++ src/main/java/com/namriahc/Rebuilding_World/registry/Rebuilding_World_Items.java
@@ -37,7 +37,7 @@ public class Rebuilding_World_Items {
     public static final RegistryObject<Item> PIONEERS_WHEAT = ITEMS.register("pioneers_wheat", Item_pioneers_wheat::new);
     public static final RegistryObject<ItemNameBlockItem> PIONEERS_WHEAT_SEEDS = ITEMS.register("pioneers_wheat_seeds", Item_pioneers_wheat_seeds::new);
 
-    public static final RegistryObject<Item> STEELY_TREE_SEED_FOSSIL = ITEMS.register("steely_tree_seed_fossil", Item_steely_tree_seed_fossil::new);
+    public static final RegistryObject<ItemNameBlockItem> STEELY_TREE_SEED_FOSSIL = ITEMS.register("steely_tree_seed_fossil", Item_steely_tree_seed_fossil::new);
 
 
 }
