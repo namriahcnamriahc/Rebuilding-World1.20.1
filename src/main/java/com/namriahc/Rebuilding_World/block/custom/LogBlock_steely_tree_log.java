11d7c555328b179a5c1571c2ff43cd9d0811a3db
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2025-02-09 00:23:45 +0900

    Add steely_tree_log series ep.1

---
+++ src/main/java/com/namriahc/Rebuilding_World/block/custom/LogBlock_steely_tree_log.java
@@ -0,0 +1,12 @@
+package com.namriahc.Rebuilding_World.block.custom;
+
+import net.minecraft.world.level.block.RotatedPillarBlock;
+
+public class LogBlock_steely_tree_log extends RotatedPillarBlock {
+    public LogBlock_steely_tree_log() {
+        super(Properties.of()
+                .strength(30F)
+                .explosionResistance(200)
+        );
+    }
+}
