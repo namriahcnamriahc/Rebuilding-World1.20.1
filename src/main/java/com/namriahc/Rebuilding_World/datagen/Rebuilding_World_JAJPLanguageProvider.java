5c2dce7a8b0e233930f309b5c55ac4658ccc9e30
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2025-02-09 13:08:15 +0900

    Add steely_tree_log series ep.6

--- src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_JAJPLanguageProvider.java
+++ src/main/java/com/namriahc/Rebuilding_World/datagen/Rebuilding_World_JAJPLanguageProvider.java
@@ -38,6 +38,7 @@ public class Rebuilding_World_JAJPLanguageProvider extends LanguageProvider {
         addBlock(Rebuilding_World_Blocks.Blocks.OVERCAST_ROCK, "曇天石");
         addBlock(Rebuilding_World_Blocks.Blocks.SPONGE_SAND, "スポンジ沙");
         addBlock(Rebuilding_World_Blocks.Blocks.WET_SPONGE_SAND, "水を吸ったスポンジ沙");
+        addBlock(Rebuilding_World_Blocks.Blocks.STEELY_TREE_LOG, "鎧樹の原木");
 
 
         addBlock(Rebuilding_World_Blocks.Blocks.OVERCAST_STONE_STEELY_TREE_SEED_FOSSIL,"曇天石中の鎧樹の種の化石");
