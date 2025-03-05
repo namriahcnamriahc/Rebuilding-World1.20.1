0b84f29b501c8f050ad469c1c3641dbb6ca7889d
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2025-03-06 00:55:15 +0900

    Add steely_tree_log series ep.12

--- src/main/java/com/namriahc/Rebuilding_World/block/custom/LogBlock_steely_tree_log.java
+++ src/main/java/com/namriahc/Rebuilding_World/block/custom/LogBlock_steely_tree_log.java
@@ -1,12 +1,14 @@
 package com.namriahc.Rebuilding_World.block.custom;
 
 
+import com.namriahc.Rebuilding_World.registry.Rebuilding_World_Blocks;
+
 public class LogBlock_steely_tree_log extends Strippable_Log_Block {
     public LogBlock_steely_tree_log() {
         super(Properties.of()
                 .strength(30F)
                 .explosionResistance(200F),
-                StrippedLogBlock_stripped_steely_tree_log::new
+                Rebuilding_World_Blocks.Blocks.STRIPPED_STEELY_TREE_LOG
         );
     }
 }
