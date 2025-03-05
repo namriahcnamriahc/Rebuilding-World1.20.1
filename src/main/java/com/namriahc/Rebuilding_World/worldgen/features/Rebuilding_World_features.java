7e2e37d007ec4c2af8750fb9813eb50fad869550
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2025-03-06 02:04:42 +0900

    Add steely_tree_log series ep.13

---
+++ src/main/java/com/namriahc/Rebuilding_World/worldgen/features/Rebuilding_World_features.java
@@ -0,0 +1,20 @@
+package com.namriahc.Rebuilding_World.worldgen.features;
+
+import com.namriahc.Rebuilding_World.main.Rebuilding_World;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+
+public class Rebuilding_World_features {
+
+    public static final ResourceKey<ConfiguredFeature<?,?>> AAA =
+            createKey("aaa")
+
+    public static ResourceKey<ConfiguredFeature<?,?>>createKey(String name) {
+
+
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE
+                ,new ResourceLocation(Rebuilding_World.MOD_ID,name));
+    }
+}
