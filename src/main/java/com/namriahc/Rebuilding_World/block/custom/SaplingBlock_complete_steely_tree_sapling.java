e39fc29e8f0807e868833a174234f1ff7f206979
namriahcnamriahc <143316668+namriahcnamriahc@users.noreply.github.com>
2026-07-04 19:42:21 +0900

    Add steely_tree_log series ep.18

--- src/main/java/com/namriahc/Rebuilding_World/block/custom/SaplingBlock_complete_steely_tree_sapling.java
+++ src/main/java/com/namriahc/Rebuilding_World/block/custom/SaplingBlock_complete_steely_tree_sapling.java
@@ -2,11 +2,12 @@ package com.namriahc.Rebuilding_World.block.custom;
 
 import com.namriahc.Rebuilding_World.worldgen.tree.Glower_steely_tree_glower;
 import net.minecraft.world.level.block.SaplingBlock;
-import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 
 public class SaplingBlock_complete_steely_tree_sapling extends SaplingBlock {
     public SaplingBlock_complete_steely_tree_sapling() {
         super(new Glower_steely_tree_glower()
-                , Properties.of().strength(30F));
+                , Properties.of()
+                        .strength(30F)
+        );
     }
 }
